ca7d5f92f8488ba71bee5359ef9549ad09be664f
YifansCode0506 <yifan.ky0523@outlook.com>
2026-07-13 18:29:39 +0930

    how to combinate with AI

---
+++ Chapter - 1/HelloWorld/src/com/yifan/test/test.java
@@ -0,0 +1,24 @@
+package com.yifan.test;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        // 彩票中奖案例，生成一个7位的随机数表示彩票号码，键盘录入一个7位数表示用户购买的彩票
+        // 判断用户输入的彩票号码是否和系统生成的彩票号码一致
+
+        // 1.生成一个7位的随机数表示彩票号码
+        int num = (int)(Math.random()*1000000);
+        // 2.键盘录入一个7位数表示用户购买的彩票
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lottery number: " + num);
+        int inputNum = sc.nextInt();
+        // 3.判断用户输入的彩票号码是否和系统生成的彩票号码一致
+        if (inputNum == num) {
+            System.out.print("恭喜你，中奖了");
+        }else {
+            System.out.print("很遗憾，没有中奖");
+        }
+    }
+}
