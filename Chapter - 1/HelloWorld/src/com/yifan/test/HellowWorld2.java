dbb2b8e6ff8fe7b26366abc12681c62b93ae395d
YifansCode0506 <yifan.ky0523@outlook.com>
2026-07-13 18:10:00 +0930

    code explain

---
+++ Chapter - 1/HelloWorld/src/com/yifan/test/HellowWorld2.java
@@ -0,0 +1,20 @@
+package com.yifan.test;
+// package:表示当前的类定义在那个包下
+// 这段代码不需要我们自己写，是IDEA自动生成的
+
+// 表示定义一个类
+// class:表示定义一个类。类是Java项目中最基本的组成单元
+// HelloWorld2:类的名字
+// {}:表示类的范围，所有的代码都需要写在这个大括号当中
+public class HellowWorld2 {
+    // 表示Java程序的主入口，当程序开始运行的时候，会从主入口开始逐行往下执行
+    // 他是一个固定格式
+    public static void main(String[] args) {
+        // 输出语句/打印语句，表示将里面的内容输出到控制台
+        System.out.println("Hello World");
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
+        System.out.println("444");
+    }
+}
