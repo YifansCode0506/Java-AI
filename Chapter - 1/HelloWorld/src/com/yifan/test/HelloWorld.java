52093f067b6b9d4e5d7f7617c72620f169a4845e
YifansCode0506 <yifan.ky0523@outlook.com>
2026-07-13 17:54:02 +0930

    start learning java

---
+++ Chapter - 1/HelloWorld/src/com/yifan/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.yifan.test;
+
+public class HelloWorld {
+    public static void main(String[] args){
+//        细节一:
+//          修改注释的颜色
+//        细节二:
+//          注释的快捷键
+//          单行注释快捷键:ctrl + /
+//          多行注释快捷键 ctrl + shift + /
+//        细节三:
+//          注释的运行规则
+//        细节四:
+//          注释的嵌套
+        System.out.println("Hello World");
+    }
+}
