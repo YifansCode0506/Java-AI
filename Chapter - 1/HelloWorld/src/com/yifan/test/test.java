db4238a57d3e25056937c254b0bbd9a099aebc43
YifansCode0506 <yifan.ky0523@outlook.com>
2026-07-13 18:31:58 +0930

    change test content

--- Chapter - 1/HelloWorld/src/com/yifan/test/test.java
+++ Chapter - 1/HelloWorld/src/com/yifan/test/test.java
@@ -9,15 +9,16 @@ public class test {
 
         // 1.生成一个7位的随机数表示彩票号码
         int num = (int)(Math.random()*1000000);
-        // 2.键盘录入一个7位数表示用户购买的彩票
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Lottery number: " + num);
-        int inputNum = sc.nextInt();
-        // 3.判断用户输入的彩票号码是否和系统生成的彩票号码一致
-        if (inputNum == num) {
-            System.out.print("恭喜你，中奖了");
-        }else {
-            System.out.print("很遗憾，没有中奖");
+        try (// 2.键盘录入一个7位数表示用户购买的彩票
+        Scanner sc = new Scanner(System.in)) {
+            System.out.println("Lottery number: " + num);
+            int inputNum = sc.nextInt();
+            // 3.判断用户输入的彩票号码是否和系统生成的彩票号码一致
+            if (inputNum == num) {
+                System.out.print("恭喜你，中奖了");
+            }else {
+                System.out.print("很遗憾，没有中奖");
+            }
         }
     }
 }
