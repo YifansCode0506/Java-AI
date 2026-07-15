0494f2be35c87aa9ff8b2c44f0c2249ac2c5b7ae
YifansCode0506 <yifan.ky0523@outlook.com>
2026-07-15 18:26:55 +0930

    rename

--- Chapter - 1/2. Basic_Code/src/com/yifan/variable/Scanner.java
+++ Chapter - 1/2. Basic_Code/src/com/yifan/variable/Scanner.java
@@ -2,19 +2,20 @@ package com.yifan.variable;
 
 public class Scanner {
     public static void main(String[] args) {
-        // 键盘录入
-        // 找到scanner
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        // 执行scanner
-        int num = sc.nextInt(); // 接收整数
-        System.out.println(num);
+        try (// 键盘录入
+                // 找到scanner
+        java.util.Scanner sc = new java.util.Scanner(System.in)) {
+            // 执行scanner
+            int num = sc.nextInt(); // 接收整数
+            System.out.println(num);
 
-        // 接收小数
-        double num2 = sc.nextDouble();
-        System.out.println(num2);
+            // 接收小数
+            double num2 = sc.nextDouble();
+            System.out.println(num2);
 
-        // 接收文本
-        String context = sc.next();
-        System.out.println(context);
+            // 接收文本
+            String context = sc.next();
+            System.out.println(context);
+        }
     }
 }
