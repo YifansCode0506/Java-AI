184c66a3accfbaa3b3c48b218aeacef0100d027b
YifansCode0506 <yifan.ky0523@outlook.com>
2026-07-15 18:50:46 +0930

    string trans

---
+++ Chapter - 1/2. Basic_Code/src/com/yifan/operator/OperatorDemo.java
@@ -0,0 +1,17 @@
+package com.yifan.operator;
+
+public class OperatorDemo {
+    public static void main(String[] args) {
+        // 实现字母的大小写转换，将大写字母转化为小写字母
+        // A ---> a
+
+        // 1. 定义变量记录大写的字符
+        char a = 'A';
+
+        // 2. 转成小写
+        // 65 + 32 = 97 ASC II
+        char cc = (char)(a + 32);
+        System.out.println(a);
+        System.out.println(cc);
+    }
+}
