7c287ca37356bcc24a9e8c5d8e620c0dba5c071d
YifansCode0506 <yifan.ky0523@outlook.com>
2026-08-07 18:14:32 +0930

    final 2

---
+++ Chapter - 2/OOPAdvanced/src/com/yifan/FinalTest2/FinalTest.java
@@ -0,0 +1,11 @@
+package com.yifan.FinalTest2;
+
+import com.yifan.FinalTest2.Student;
+
+public class FinalTest {
+    public static void main(String[] args) {
+        final Student STU = new Student();
+        // STU.name = "aaa";
+        System.out.println(STU.getName());
+    }
+}
---
+++ Chapter - 2/OOPAdvanced/src/com/yifan/FinalTest2/Student.java
@@ -0,0 +1,18 @@
+package com.yifan.FinalTest2;
+
+public class Student {
+    private final String name = "张三";
+    private final int age = 23;
+
+    public Student() {
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
