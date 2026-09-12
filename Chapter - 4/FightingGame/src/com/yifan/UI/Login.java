0d899b3c6ad62ead6c8e3a4c286c936da5f049f7
YifansCode0506 <yifan.ky0523@outlook.com>
2026-09-12 15:43:56 +0930

    FightGame_Register_create User

--- Chapter - 4/FightingGame/src/com/yifan/UI/Login.java
+++ Chapter - 4/FightingGame/src/com/yifan/UI/Login.java
@@ -1,5 +1,8 @@
 package com.yifan.UI;
 
+import com.yifan.domain.User;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Login {
@@ -8,16 +11,20 @@ public class Login {
     public void start() {
         System.out.println("游戏的登录注册页面打开了");
 
+        ArrayList<User> list = new ArrayList<>();
+
         while(true) {
+            System.out.println("==================");
             System.out.println("欢迎来到文字格斗游戏");
+            System.out.println("==================");
             System.out.println("请选择操作：1登录 2注册 3退出");
 
             Scanner sc = new Scanner(System.in);
             String choose = sc.next();
 
             switch (choose) {
-                case "1" -> login();
-                case "2" -> register();
+                case "1" -> login(list);
+                case "2" -> register(list);
                 case "3" -> {
                     System.out.println("用户选择了退出操作");
                     System.exit(0);
@@ -28,12 +35,12 @@ public class Login {
     }
 
     // 登录操作
-    public void login() {
+    public void login(ArrayList<User> list) {
         System.out.println("用户选择了登录操作");
     }
 
     // 注册操作
-    public void register() {
+    public void register(ArrayList<User> list) {
         System.out.println("用户选择了登录操作");
     }
 
---
+++ Chapter - 4/FightingGame/src/com/yifan/domain/User.java
@@ -0,0 +1,68 @@
+package com.yifan.domain;
+
+import java.util.Random;
+
+public class User {
+    // id，用户名，密码，状态
+    private String id;
+    private String username;
+    private String password;
+    private boolean status;
+
+    public User(){
+        // 调用 createID 方法
+        id = createID();
+        // 修改status数值
+        status = true;
+    }
+    public User(String username, String password) {
+        id = createID();
+        this.username = username;
+        this.password = password;
+        status = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.username = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    // 用户无法设置，是自动生成的，格式为:Fight+5位数字的随机数
+    public String createID(){
+        StringBuilder b = new StringBuilder("Fight");
+
+        Random r = new Random();
+        for (int i = 0; i < 5; i++) {
+            int num = r.nextInt(10);
+            b.append(num);
+        }
+        return b.toString();
+    }
+}
